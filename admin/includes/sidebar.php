e6292bb780f86cd6aa2940ec93f3f3aa1e005a0d
upendri <awekshikaupendri3129@gmail.com>
2025-10-05 18:37:41 +0530

    day 2

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,10 @@
+<aside class="sidebar">
+  <div class="logo">EDUMA Admin</div>
+  <nav class="sidebar-nav">
+    <a href="dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+    <a href="manage-instructors.php"><i class="fas fa-users"></i> Manage Instructors</a>
+    <a href="manage-categories.php"><i class="fas fa-file"></i> Manage Categories</a>
+    <a href="change-password.php"><i class="fas fa-key"></i> Change Password</a>
+    <a href="../logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+  </nav>
+</aside>
